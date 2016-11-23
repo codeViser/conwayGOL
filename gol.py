f6af2d8d731e6da4fc796ec3e22937814910f868
SHUBHAM CHOWDHARY <shubham.note3@gmail.com>
2016-11-24 00:01:47 +0530

    Calculating LifeTime for any Design
    This is an extension to the CGOL, where any image showing a pattern is tested for the duration for which it exhibits variation according to the game of life rules. The final score is calculated on the basis of 2 factors:
    1. If the design continues to repeat itself the score ceases to increase
    2. If the design reaches to 98% of its steady state value the game stops.

--- gol.py
+++ gol.py
@@ -1,22 +1,33 @@
 import tensorflow as tf
 from matplotlib import pyplot as plt
+from PIL import Image
 import numpy as np
 from scipy.signal import convolve2d
 import matplotlib.animation as animation
+import matplotlib.image as mpimg
+import matplotlib.patches as patches
+import time
 
 
-shape = (50, 50)
-initial_board = tf.random_uniform(shape, minval=0, maxval=2, dtype=tf.int32)
+img = Image.open('testImage4.png').convert('L')
+data = np.asarray(img.getdata()).reshape(img.size)
+shape = data.shape
+data = data < 128;
+#custInp=np.loadtxt(open("customInput2.csv","rb"),delimiter=",",skiprows=0)
+# initial_board = tf.random_uniform(shape, minval=0, maxval=2, dtype=tf.int32)
+initial_board = tf.constant(data, dtype=tf.int32)
 
 with tf.Session() as session:
-    X = session.run(initial_board)
+	print(shape)
+	print(session.run(initial_board))
+	X = session.run(initial_board)
 
 fig = plt.figure()
-plot = plt.imshow(X, cmap='Greys',  interpolation='nearest')
+plot = plt.imshow(X, cmap='PuBu',  interpolation='nearest')
 #plt.show()
 
 
-
+start_time = time.time()
 
 def update_board(X):
     # Check out the details at: https://jakevdp.github.io/blog/2013/08/07/conways-game-of-life/
@@ -40,18 +51,30 @@ with tf.Session() as session:
     X = session.run(board_update, feed_dict={board: initial_board_values})[0]
 
 
-
+sizeNet = np.prod(data.shape)
 def game_of_life(*args):
 	global X
 	with tf.Session() as session:
 		Y = session.run(board_update, feed_dict={board: X})[0]
 		Z = session.run(board_update, feed_dict={board: Y})[0]
+		diff =  np.count_nonzero(X==Z)
+		if (diff > 0.98*sizeNet):
+			print("--- %s SCORE ---" % (time.time() - start_time))
+			time.sleep(30)
+			exit(0)
+
 		if np.array_equal(X,Z):
-			initial_board_values = session.run(initial_board)
-			X = session.run(board_update, feed_dict={board: initial_board_values})[0]
-			Y=X
+			print("--- %s SCORE ---" % (time.time() - start_time))
+			time.sleep(30)
+			exit(0)
+
+
+			# initial_board_values = session.run(initial_board)
+			# X = session.run(board_update, feed_dict={board: initial_board_values})[0]
+			# Y=X
 
 		X=Y
+		print("--- %s SCORE ---" % (time.time() - start_time))
 		plot.set_array(Y)
 	return plot,
 
--- initial_board.py
+++ initial_board.py
@@ -1,12 +1,21 @@
 import tensorflow as tf
+from PIL import Image
+import numpy as np
 from matplotlib import pyplot as plt
 
-shape = (50, 50)
-initial_board = tf.random_uniform(shape, minval=0, maxval=2, dtype=tf.int32)
+img = Image.open('testImage4.png').convert('L')
+data = np.asarray(img.getdata()).reshape(img.size)
+shape = data.shape
+data = data < 128;
+#custInp=np.loadtxt(open("customInput2.csv","rb"),delimiter=",",skiprows=0)
+# initial_board = tf.random_uniform(shape, minval=0, maxval=2, dtype=tf.int32)
+initial_board = tf.constant(data, dtype=tf.int32)
 
 with tf.Session() as session:
-    X = session.run(initial_board)
+	print(shape)
+	print(session.run(initial_board))
+	X = session.run(initial_board)
 
 fig = plt.figure()
-plot = plt.imshow(X, cmap='Greys',  interpolation='nearest')
+plot = plt.imshow(X, cmap='PuBu',  interpolation='nearest')
 plt.show()
