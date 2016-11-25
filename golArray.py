5aaef61467a4b276518601d86fe7452a5d2aee8b
SHUBHAM CHOWDHARY <shubham.note3@gmail.com>
2016-11-25 11:35:55 +0530

    Both custom array & images
    Programmed to accept images as well as custom arrays

--- golArray.py
+++ golArray.py
@@ -10,25 +10,28 @@ import time
 
 
 #INITIALIZE BOARD BY IMAGE
-img = Image.open('testImage3.png').convert('L')
-data = np.asarray(img.getdata()).reshape(img.size)
-shape = data.shape
-data = data < 128;
-
-
-# #INITIALIZE BOARD BY CUSTOM MATRIX
-# # data=[[0,0,1,0,0],[0,1,0,1,0],[0,1,1,1,0],[0,1,0,1,0],[0,1,0,1,0]]
-# # shape = (5,5)
-# data=np.zeros((30,25))
-# data[12][8]=data[12][9]=data[12][10]=data[12][12]=1
-# data[13][8]=1
-# data[14][11]=data[14][12]=1
-# data[15][9]=data[15][10]=data[15][12]=1
-# data[16][8]=data[16][10]=data[16][12]=1
+# img = Image.open('testImageX.png').convert('L')
+# data = np.asarray(img.getdata()).reshape(img.size)
 # shape = data.shape
+# data = data < 128;
+
+
+#INITIALIZE BOARD BY CUSTOM MATRIX
+# data=[[0,0,1,0,0],[0,1,0,1,0],[0,1,1,1,0],[0,1,0,1,0],[0,1,0,1,0]]
+# shape = (5,5)
+data=np.zeros((30,25))
+data[12][8]=data[12][9]=data[12][10]=data[12][12]=1
+data[13][8]=1
+data[14][11]=data[14][12]=1
+data[15][9]=data[15][10]=data[15][12]=1
+data[16][8]=data[16][10]=data[16][12]=1
+shape = data.shape
+
+
 
 #INITIALIZE BOARD RANDOMLY
 # initial_board = tf.random_uniform(shape, minval=0, maxval=2, dtype=tf.int32)
+
 
 #PROCESSING
 initial_board = tf.constant(data, dtype=tf.int32)
